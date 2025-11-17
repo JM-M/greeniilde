3e7e481a6d07bdad8a8784d3c5140f5310170f19
Michael Jola-Moses <mikejolamoses@gmail.com>
2025-11-17 23:35:28 +0100

    improve product grid responsivity

--- app/modules/products/ui/components/product-grid.tsx
+++ app/modules/products/ui/components/product-grid.tsx
@@ -106,7 +106,7 @@ const products = [
 
 export const ProductGrid = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       {products.map((product, index) => (
         <Link key={index} href={`/products/1`}>
           <ProductCard
--- app/modules/products/ui/components/sort-dialog.tsx
+++ app/modules/products/ui/components/sort-dialog.tsx
@@ -20,4 +20,3 @@ export const SortDialog = ({ open, onOpenChange }: SortDialogProps) => {
     </ResponsiveDialog>
   );
 };
-
